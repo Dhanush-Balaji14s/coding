f1c92bc1f765f874f8388769148a5ac3cbfbd076
Dhanush-Balaji14s <dhanushbalaji14s@gmail.com>
2026-02-19 21:30:55 +0530

    Add multiplication table for 5 in Main class

---
+++ tables.java
@@ -0,0 +1,12 @@
+
+public class Main{
+  public static void main(String[] args){
+      int i=1;
+      while(i<=10)
+      {
+          System.out.println("5 X " + i + " = " + (5*i));
+          i++;
+      }
+    //for(int i=1;i<=10;i++){
+     // System.out.println("5 X " + i + " = " + (5 * i));
+  }}
