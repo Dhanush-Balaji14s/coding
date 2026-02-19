d34c8e3e881d7f036244edc293dc352c6c1ce3d1
Dhanush-Balaji14s <dhanushbalaji14s@gmail.com>
2026-02-19 10:46:46 +0530

    Create num_1to10.java to print 'Balaji' ten times
    Prints 'Balaji' ten times using a loop.

---
+++ num_1to10.java
@@ -0,0 +1,20 @@
+public class main{
+  public static void main(String[] args)
+  {
+    for(int i=1;i<=10;i++){
+      System.out.println("Balaji");
+    }
+  }
+} 
+output:
+Balaji
+Balaji
+Balaji
+Balaji
+Balaji
+Balaji
+Balaji
+Balaji
+Balaji
+Balaji
+  
