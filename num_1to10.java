541d23731395d029bfb0293a63b4f072f5583bbf
Dhanush-Balaji14s <dhanushbalaji14s@gmail.com>
2026-02-19 10:52:50 +0530

    Fix loop to print 'Balaji' 10 times
    Corrected loop structure and added comments for clarity.

--- num_1to10.java
+++ num_1to10.java
@@ -4,8 +4,8 @@ public class main{
     for(int i=1;i<=10;i++){
       System.out.println("Balaji");
     }
-  }
-} 
+  }  //for(int i=1;i<=10;i++)  1 2 3 4 5 6 7 8 9 10
+} // for(int i=10;i<=1;i++) 10 9 8 7 6 5 4 3 2 1
 output:
 Balaji
 Balaji
