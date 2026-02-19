ffe68bb7e49aab274e8c1f5af715cd3848632de4
Dhanush-Balaji14s <dhanushbalaji14s@gmail.com>
2026-02-19 11:04:18 +0530

    condition 1 to 20  prints the even and odd number
    Fixed class name and updated print statement for even numbers.

---
+++ even_1to20.java
@@ -0,0 +1,12 @@
+public class even_1to20.java
+  {
+    public static void main(String[] args){
+      for(int i=1;i<=20;i++){
+        if(i%2==0){                //if(i%2!=0) 1 to 20 print the even number
+          System.out.print(i+" ");  1to20 i%2==0 prints the even number
+        }
+      }
+    }
+  }
+output
+  2 4 6 8 10 12 14 16 18 20
